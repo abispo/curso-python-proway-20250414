843d796c35afcc30f02b06e73f4969617d2823eb
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-04-14 21:02:18 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20250414-aula01/prog04.py
@@ -0,0 +1,11 @@
+# Tipos de dados
+
+# Booleano
+# O tipo de dado booleano (bool) caracteriza-se por ter apenas 2 valores: Ou verdadeiro (True) ou falso (False)
+
+if __name__ == "__main__":
+    print(5 > 4)    # 5 é maior que 4?
+    print(1 != 3)   # 1 é diferente de 3?
+    print(bool(0))  # Conversão de 0 para bool
+
+    # As expressões com operadores de comparação, lógicos, de identidade e de pertencimento sempre irão retornar um tipo booleano.
