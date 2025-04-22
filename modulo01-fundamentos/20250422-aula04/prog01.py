eace7d2294c23f5dce96ed711bee01e746f66e95
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-04-22 18:47:20 -0300

    arquivos aula 04 python fundamentos

--- modulo01-fundamentos/20250422-aula04/prog01.py
+++ modulo01-fundamentos/20250422-aula04/prog01.py
@@ -11,3 +11,15 @@ if __name__ == "__main__":
     """
 
     arquivo = open(file="linguagens.txt", mode="r")
+
+    """
+    Como mencionado acima, o parâmetro file é obrigatório. Aqui indicamos o caminho/nome do arquivo. Caso o arquivo esteja no mesmo diretório que o script python que está sendo executado, basta passar apenas o nome do arquivo.
+
+    O parâmetro mode, apesar de ser opcional, é muito importante. Aqui definimos o modo de abertura do arquivo. Caso não informemos o valor, por padrão ele recebe 'r' (read only/somente leitura)
+
+    Abaixo estão os modos possíveis de se abrir um arquivo. Não há problema em mesclá-los:
+
+    r   -> Read Only/Modo somente leitura. Não conseguimos fazer modificações no arquivo, apenas ler o seu conteúdo.
+    w   -> Write Only/Somente escrita. Nesse modo, caso o arquivo não exista, ele será criado. E caso ele exista, o seu conteúdo é sobreposto (truncado) com o nome conteúdo inserido.
+    a   -> Append/Adicionar ao final. Caso o arquivo não exista, ele será criado. E caso exista, todo novo conteúdo inserido será colocado a partir do final do arquivo.
+    """
