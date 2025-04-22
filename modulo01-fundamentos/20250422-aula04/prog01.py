aa8b5d78d2bea69378eabf053c7c6a2cd113f012
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-04-22 19:30:55 -0300

    Arquivos aula 04 python fundamentos

--- modulo01-fundamentos/20250422-aula04/prog01.py
+++ modulo01-fundamentos/20250422-aula04/prog01.py
@@ -22,4 +22,40 @@ if __name__ == "__main__":
     r   -> Read Only/Modo somente leitura. Não conseguimos fazer modificações no arquivo, apenas ler o seu conteúdo.
     w   -> Write Only/Somente escrita. Nesse modo, caso o arquivo não exista, ele será criado. E caso ele exista, o seu conteúdo é sobreposto (truncado) com o nome conteúdo inserido.
     a   -> Append/Adicionar ao final. Caso o arquivo não exista, ele será criado. E caso exista, todo novo conteúdo inserido será colocado a partir do final do arquivo.
+    r+  -> Indicamos que estamos abrindo o arquivo para leitura e escrita ao mesmo tempo
+
+    E além disso, podemos indicar também o tipo de arquivo que estamos abrindo: arquivo somente texto(t) e arquivo binário (b). Por padrão, se não informarmos, a função open() irá assumir que um arquivo texto está sendo aberto. Para indicarmos o tipo de arquivo, passamos junto com o modo. Exemplo:
+
+    open("notas.txt", rt)       # Estamos abrindo o arquivo notas.txt, indicando que é um arquivo texto que será aberto no modo somente leitura
+    open("virus.exe", ab)       # Estamos abrindo o arquivo virus.exe, indicando que é um arquivo binário e que será aberto no modo append.
     """
+
+    """
+    Podemos abrir um arquivo Python de maneiras diferentes. Quando abrimos no modo somente leitura, temos acesso aos seguintes métodos
+    """
+    print(arquivo.read(10))
+    # O método read() lê o conteúdo do arquivo e retorna como uma string. Caso não informemos o primeiro parâmetro, esse método irá ler o arquivo inteiro.
+
+    print(arquivo.readline(20))
+    # O método readline lê uma linha por vez, até chegar no caractere oculto newline. Não importa o valor que passemos para o parâmetro size, o método lerá apenas até o final da linha. Caso não passemos nada, ele lê a linha inteira.
+    
+    print(arquivo.readline())
+    # Agora na linha acima, todo o conteúdo da próxima linha será lido, até chegar no caractere nova linha, não importando quantos caracteres existam nessa linha.
+
+    print(arquivo.readlines(11))
+    # O método readlines() lê o conteúdo do arquivo e retorna uma lista de strings, com cada string representando uma linha do arquivo. Caso o cursor pare antes do final da linha, o método readlines() pega a linha toda do mesmo jeito.
+
+    print('*'*20)
+
+    print(arquivo.readlines())
+    # Como o cursor está no final do arquivo, qualquer chamada para ler o conteúdo, vai resultar no retorno de uma string ou de uma lista vazia
+
+    print("Posição do cursor: {}".format(arquivo.tell()))
+    # O método tell() indica a posição atual do cursor.
+    # Caso queiramos retornar ao início do arquivo, utilizamos o método seek()
+
+    arquivo.seek(0)
+    # Acima, estamos reposicionando o cursor na posição 0 do arquivo. Assim, podemos lê-lo novamente
+
+    print(arquivo.readlines())
+    # Mostramos novamente todo o conteúdo do arquivo,
