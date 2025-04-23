48114d70275045f3ec4f324e3cc339d62f66f084
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-04-23 20:50:31 -0300

    Arquivos aula 05 python fundamentos

--- modulo01-fundamentos/20250423-aula05/prog02.py
+++ modulo01-fundamentos/20250423-aula05/prog02.py
@@ -1,10 +1,25 @@
 
+from requests.exceptions import ConnectionError
+
 import requests
 
-# https://pastebin.com/WsL62aGY
-
-BASE_URL = ""
 access_token = ""
+BASE_URL = "https://superheroapi.com/api/{}".format(access_token)
 
 if __name__ == "__main__":
-    pass
+
+    try:
+        r = requests.get(
+            "{}/{}".format(BASE_URL, 423)
+        )
+
+        print(r.json())
+
+    except Exception as exc_info:
+        print("Aconteceu um erro: {}".format(str(exc_info)))
+
+    else:
+        print("Requisição realizada com sucesso!")
+
+    finally:
+        print("Fechando conexão...")
