78956b1ea66a085dc6abcb70c3490e2784d3a137
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2025-04-30 21:44:55 -0300

    Arquivos aula 05 python com banco de dados

--- modulo02-banco-de-dados/20250430-aula05/usuarios.py
+++ modulo02-banco-de-dados/20250430-aula05/usuarios.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import select
 
 from config import session
@@ -15,7 +17,7 @@ def gerenciar_usuarios():
                 break
 
             case 2:
-                pass
+                cadastrar_usuario()
 
             case 3:
                 pass
@@ -28,3 +30,36 @@ def gerenciar_usuarios():
 
             case _:
                 print(f"Opção '{opcao}' inválida")
+
+
+def cadastrar_usuario():
+
+    # Pedimos as informações de usuário
+    email = input("Informe o e-mail do usuário: ")
+    senha = input("Informe a senha do usuário: ")
+    nome = input("Informe o nome do usuário: ")
+    data_de_nascimento = input("Informe a data de nascimento (dd/mm/yyyy)")
+
+    # Instanciamos a classe Usuario passando o e-mail e a senha
+    usuario = Usuario(email=email, senha=senha)
+
+    # Adicionamos esse objeto na sessão. Ele ficará "enfileirado", até confirmarmos a transação
+    session.add(usuario)
+
+    # Confirmamos a transação com o commit(). Todas as operações que estiverem pendentes na sessão, serão executadas
+    session.commit()
+
+    # A partir de usuario, criamos o perfil desse usuario
+    perfil = Perfil(
+        id=usuario.id,
+        nome=nome,
+        data_de_nascimento=datetime.strptime(
+            data_de_nascimento, "%d/%m/%Y"      # Aqui convertemos a string em uma data
+        ).date()
+    )
+
+    # Adicionamos o perfil à sessão e depois confirmamos a transação
+    session.add(perfil)
+    session.commit()
+
+    print(f"Usuário {perfil.nome}({usuario.email}) cadastrado com sucesso!")
